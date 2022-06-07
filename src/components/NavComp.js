6a37d5d931d80a7444a0a167032dbf5aafec68a3
Sherrisa Classon <sherrisaclasson@outlook.com>
2022-06-06 23:12:11 -0700

    June 6

---
+++ src/components/NavComp.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "../pages/home";
+import Today from "../pages/today";
+
+function NavComp() {
+  return (
+    <div>
+      <Navbar bg="dark" variant={"dark"} expand="lg">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/">
+            Daily 100
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={Link} to="/">
+                Home
+              </Nav.Link>
+              <Nav.Link as={Link} to="/today">
+                Today's Project
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/today" element={<Today />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default NavComp;
---
+++ src/pages/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div><h1>Welcome to My Daily 100 Project</h1></div>;
+}
+
+export default Home;
